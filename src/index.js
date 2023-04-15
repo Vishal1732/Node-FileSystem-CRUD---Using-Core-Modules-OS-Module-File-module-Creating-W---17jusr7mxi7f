ef054ebd86cdfa12b46bbdf58254869e519c18bf
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-04-15 09:13:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
-	const myFileReader async(fileName, fileContent);
+        await fs.writeFile(fileName, fileContent)
 }
 
 const myFileReader = async (fileName) => {
